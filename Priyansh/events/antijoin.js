ca156c3ca716d9a343b54ffa85edbc49951e63de
XDEMONONFORE <djjinsidea@gmail.com>
2025-07-21 15:29:11 +0500

    Update antijoin.js

--- Priyansh/events/antijoin.js
+++ Priyansh/events/antijoin.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "antijoin",
  eventType: ["log:subscribe"],
  version: "1.0.0",
- credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+ credits: "꧁𓊈𒆜🆂🅷🅴🆁🅰🆉🆇🅳🅹𒆜𓊉꧂",
  description: "Welcome new members to the group"
 };
 
