1a43721cf0c45250d2afc5b7f36ee5a2a6cca235
XDEMONONFORE <djjinsidea@gmail.com>
2025-07-21 15:33:14 +0500

    Update autosetname.js

--- Priyansh/events/autosetname.js
+++ Priyansh/events/autosetname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "autosetname",
 	eventType: ["log:subscribe"],
 	version: "1.0.3",
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "꧁𓊈𒆜🆂🅷🅴🆁🅰🆉🆇🅳🅹𒆜𓊉꧂",
 	description: "Automatically set new member nicknames"
 };
 
